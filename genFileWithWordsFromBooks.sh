14f00bd965fe7e5d3f0feb8efc24f51cdd716f72
Kunakbaev <tyrep2017@mail.ru>
2025-04-07 13:42:12 +0300

    script to generate list of words from books

---
+++ genFileWithWordsFromBooks.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+set -eu
+
+SOURCE_FILES_DIR="sourceFiles"
+BOOKS_SOURCE_DIR="$SOURCE_FILES_DIR/books"
+FILES_WITH_WORDS_DEST_DIR="$SOURCE_FILES_DIR/words"
+ALL_WORDS_FILE_NAME="allWords"
+MIN_WORD_LEN=3
+
+echo "parsing words from books located at $BOOKS_SOURCE_DIR" 
+
+function parseWordsFromFile() {
+    grep -Eow "\w{$MIN_WORD_LEN,}" "$1" > "$2"
+}
+
+function generateAllWordFiles() {
+    for bookFileName in `ls $BOOKS_SOURCE_DIR`; do
+        echo "bookFileName : $bookFileName";
+        fileName="$BOOKS_SOURCE_DIR/$bookFileName"
+        outputFileName="$FILES_WITH_WORDS_DEST_DIR/words_$bookFileName"
+        parseWordsFromFile $fileName $outputFileName
+    done
+}
+
+function concatWordFilesIntoOne() {
+    IFS=$'\n'
+    tmp=$(ls $FILES_WITH_WORDS_DEST_DIR)
+    fileNames=()
+    #echo "fileNames: ${fileNames[@]}"
+
+    for f in ${tmp[@]}; do
+        # echo "f : $f"
+        fileNames+=("$FILES_WITH_WORDS_DEST_DIR/$f")
+    done
+
+    cat "${fileNames[@]}" > "$SOURCE_FILES_DIR/"$ALL_WORDS_FILE_NAME".txt"
+}
+
+generateAllWordFiles
+
+echo "concatinating all words from all books into one file"
+concatWordFilesIntoOne
+
+echo "done"
